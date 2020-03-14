571decb24ec1f74a6223bfc7b03a400f25f5ff96
Anton Begehr <a.begehr@gmail.com>
2020-03-14 22:06:21 +0100

    preload assets after runApp()
    Exception has occurred.
    FlutterError (ServicesBinding.defaultBinaryMessenger was accessed before the binding was initialized.
    If you're running an application and need to access the binary messenger before `runApp()` has been called (for example, during plugin initialization), then you need to explicitly call the `WidgetsFlutterBinding.ensureInitialized()` first.
    If you're running a test, you can call the `TestWidgetsFlutterBinding.ensureInitialized()` as the first line in your test's `main()` method to initialize the binding.)

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
-import 'package:flame/flame.dart';
 import 'package:coronajump/game.dart';
+import 'package:flame/flame.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/util.dart';
 import 'package:flutter/services.dart';
 
 void main() {
-  Flame.images.loadAll(<String>['bg/background.png', 'virus/virus.png']);
-
   CoronaJump game = CoronaJump();
   runApp(game.widget);
+
+  Flame.images.loadAll(<String>['bg/background.png', 'virus/virus.png']);
 
   Util flameUtil = Util();
   flameUtil.fullScreen();
