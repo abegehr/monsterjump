e370edc5ae26c9ef664758692b1b58305ab07ebe
Anton Begehr <a.begehr@gmail.com>
2020-03-14 21:11:05 +0100

    import flame and settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flame/util.dart';
+import 'package:flutter/services.dart';
 
-void main() {}
+void main() async {
+  Util flameUtil = Util();
+  await flameUtil.fullScreen();
+  await flameUtil.setOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+}
