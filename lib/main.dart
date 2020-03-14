5e164b444f1fc89605c33b0ea6cfd02af8ca206c
Anton Begehr <a.begehr@gmail.com>
2020-03-14 21:46:00 +0100

    preload image assets on startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:flame/flame.dart';
 import 'package:coronajump/game.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/util.dart';
 import 'package:flutter/services.dart';
 
 void main() {
+  Flame.images.loadAll(<String>['bg/background.png', 'virus/virus.png']);
+
   CoronaJump game = CoronaJump();
   runApp(game.widget);
 
