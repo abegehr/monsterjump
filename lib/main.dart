fea04bc3223f20fdb61a85f158a61d898e523383
Anton Begehr <a.begehr@gmail.com>
2020-03-14 21:16:04 +0100

    run CoronaJump game

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:coronajump/game.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/util.dart';
 import 'package:flutter/services.dart';
 
-void main() async {
+void main() {
+  CoronaJump game = CoronaJump();
+  runApp(game.widget);
+
   Util flameUtil = Util();
-  await flameUtil.fullScreen();
-  await flameUtil.setOrientations(
+  flameUtil.fullScreen();
+  flameUtil.setOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
 }
