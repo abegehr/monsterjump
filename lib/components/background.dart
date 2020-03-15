a0d55b7af25fced6d1f3db065cd2ba844866f386
Anton Begehr <a.begehr@gmail.com>
2020-03-15 02:54:11 +0100

    no need for this, reorder imports

--- lib/components/background.dart
+++ lib/components/background.dart
@@ -1,20 +1,20 @@
 import 'dart:ui';
-import 'package:flame/anchor.dart';
 import 'package:flame/components/component.dart';
 import 'package:flame/sprite.dart';
+import 'package:flame/anchor.dart';
 
 class Background extends SpriteComponent {
   final double aspectRatio = 14073 / 1125;
 
   Background() : super.fromSprite(null, null, new Sprite('bg/background.png')) {
-    this.anchor = Anchor.bottomCenter;
-    this.x = 0;
+    anchor = Anchor.bottomCenter;
+    x = 0;
   }
 
   @override
   void resize(Size size) {
-    this.width = size.width;
-    this.height = this.aspectRatio * this.width;
-    this.y = size.height;
+    width = size.width;
+    height = aspectRatio * width;
+    y = size.height;
   }
 }
