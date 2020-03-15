316000d2e6cb5d87d143c1c51b7ce81af029bfba
Anton Begehr <a.begehr@gmail.com>
2020-03-15 17:49:39 +0100

    import order

--- lib/components/player.dart
+++ lib/components/player.dart
@@ -2,8 +2,8 @@ import 'dart:ui';
 import 'package:flame/components/component.dart';
 import 'package:flame/sprite.dart';
 import 'package:flame/anchor.dart';
-import 'package:box2d_flame/box2d.dart';
 import 'package:flame/box2d/box2d_component.dart';
+import 'package:box2d_flame/box2d.dart';
 import 'package:flutter/painting.dart';
 
 const num SIZE = 48.0;
