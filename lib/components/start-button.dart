011629ce8d4ec7a2462439f0783045930f347bbd
Adrian Pfeiffer-Ruiz <adrian.pfeiffer.ruiz@web.de>
2020-03-16 12:24:29 +0100

    added component start-button

---
+++ lib/components/start-button.dart
@@ -0,0 +1,27 @@
+import 'dart:ui';
+import 'package:flame/sprite.dart';
+import 'package:coronajump/game.dart';
+
+class StartButton {
+  final CoronaJump game;
+  Rect rect;
+  Sprite sprite;
+
+  StartButton(this.game) {
+    rect = Rect.fromLTWH(
+      game.tileSize * 1.5,
+      (game.screenSize.height * .75) - (game.tileSize * 1.5),
+      game.tileSize * 6,
+      game.tileSize * 3,
+    );
+    sprite = Sprite('ui/start-button.png');
+  }
+
+  void render(Canvas c) {
+    sprite.renderRect(c, rect);
+  }
+
+  void update(double t) {}
+
+  void onTapDown() {}
+}
