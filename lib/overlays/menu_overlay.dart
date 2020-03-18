d8866ed9d67bfb00d8c34ddff49bca4548816b22
Adrian Pfeiffer-Ruiz <adrian.pfeiffer.ruiz@web.de>
2020-03-18 02:43:12 +0100

    added styling and other stuff (PlayButton NOT WORKING!)
    they play button is not working any more

--- lib/overlays/menu_overlay.dart
+++ lib/overlays/menu_overlay.dart
@@ -7,17 +7,49 @@ class MenuOverlay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Container(
-            width: 240,
-            height: 240,
-            color: Color(0x88FF0000),
-            child: Column(children: <Widget>[
-              Text("CoronaJump"),
-              FlatButton(
-                onPressed: start,
-                child: Text("Start Game"),
-              )
-            ], mainAxisAlignment: MainAxisAlignment.center)));
+    return Stack(
+      children: <Widget>[
+        Positioned(
+          top: -180,
+          right: -200,
+          width: 500,
+          height: 500,
+          child: Container(
+            child: Image.asset(
+              'assets/images/virus/virus.png',
+            ),
+          ),
+        ),
+        Positioned(
+          top: 180,
+          left: 10,
+          child: Container(
+            width: 280,
+            height: 150,
+            child: Image.asset(
+              'assets/images/ui/title.png',
+            ),
+          ),
+        ),
+        Center(
+          child: RaisedButton(
+              onPressed: start,
+              child: Padding(
+                padding: EdgeInsets.all(12),
+                child: Text(
+                  'PLAY',
+                  style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              color: Colors.blue[500]),
+        )
+      ],
+    );
   }
+
+  onPressed() {}
 }
