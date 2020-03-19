7f00064bb22d56ee0b2841e39f707368a3721466
moweigert <53445251+moweigert@users.noreply.github.com>
2020-03-19 17:51:15 +0100

    Implementation Banner Ad

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,37 @@ void main() {
 
   FirebaseAdMob.instance.initialize(appId: FirebaseAdMob.testAppId);
 
+  MobileAdTargetingInfo targetingInfo = MobileAdTargetingInfo(
+    keywords: <String>['game', 'corona', 'awesome', 'fun'],
+    contentUrl: 'https://places.rocks',
+    childDirected: false,
+    testDevices: <String>[], // Android emulators are considered test devices
+  );
+
+  BannerAd myBanner = BannerAd(
+    // Replace the testAdUnitId with an ad unit id from the AdMob dash.
+    // https://developers.google.com/admob/android/test-ads
+    // https://developers.google.com/admob/ios/test-ads
+    adUnitId: BannerAd.testAdUnitId,
+    size: AdSize.smartBanner,
+    targetingInfo: targetingInfo,
+    listener: (MobileAdEvent event) {
+      print("BannerAd event is $event");
+    },
+  );
+
+  myBanner
+    // typically this happens well before the ad is shown
+    ..load()
+    ..show(
+      // Positions the banner ad 60 pixels from the bottom of the screen
+      anchorOffset: 0.0,
+      // Positions the banner ad 10 pixels from the center of the screen to the right
+      horizontalCenterOffset: 0.0,
+      // Banner Position
+      anchorType: AnchorType.bottom,
+    );
+
   Flame.images.loadAll(<String>[
     'bg/background.png',
     'virus/virus.png',
