0cdf836dc9923a8759b3abbb1ab03d81c7f06cb6
Tom Gülenman <30436283+tguelenman@users.noreply.github.com>
2020-03-21 13:53:11 +0100

    fix admob

--- android/app/src/main/kotlin/com/example/coronajump/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/coronajump/MainActivity.kt
@@ -4,9 +4,11 @@ import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
+import com.google.android.gms.ads.MobileAds;
 
 class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
+        MobileAds.initialize(this) {}
     }
 }
