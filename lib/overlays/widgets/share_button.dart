af1092e67589ee23e3e9867bab00b1929893408d
Anton Begehr <a.begehr@gmail.com>
2020-03-27 11:16:49 +0100

    place ahre popup at tap position on iPad

--- lib/overlays/widgets/share_button.dart
+++ lib/overlays/widgets/share_button.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:share/share.dart';
 
 class ShareButton extends StatelessWidget {
-  void onPress() {
-    print("ShareButton – pressed");
+  void onPress(TapUpDetails e) {
+    print("ShareButton – pressed: ${e.globalPosition}");
     Share.share(
       'Check out this cool game: https://monsterjump.page.link/download and stay at home!',
       sharePositionOrigin:
-          Rect.fromCenter(center: Offset(100, 100), width: 100, height: 100),
+          Rect.fromCenter(center: e.globalPosition, width: 100, height: 100),
     );
   }
 
@@ -18,7 +18,7 @@ class ShareButton extends StatelessWidget {
       child: Align(
         alignment: Alignment.bottomRight,
         child: GestureDetector(
-          onTap: onPress,
+          onTapUp: onPress,
           child: Image.asset(
             'assets/images/ui/share_button.png',
             width: 165,
