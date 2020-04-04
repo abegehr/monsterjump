11d792b5784506a2bd99567dfdd4cbe98cb3ea27
Anton Begehr <a.begehr@gmail.com>
2020-04-04 16:29:08 +0200

    runApp and setup in zone for including stackrace in Crashlytics error
    https://pub.dev/packages/firebase_crashlytics#use-the-plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:monsterjump/utils/admob.dart';
 import 'package:flutter/material.dart';
@@ -7,30 +8,38 @@ import 'package:flame/util.dart';
 import 'package:monsterjump/game.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
+  //Crashlytics.instance.enableInDevMode = true;
 
-  // Pass all uncaught errors from the framework to Crashlytics.
-  FlutterError.onError = Crashlytics.instance.recordFlutterError;
+  // on uncaught flutter error
+  FlutterError.onError = (details) {
+    print("FlutterERROR: $details");
+    return Crashlytics.instance.recordFlutterError(details);
+  };
 
-  // Admob setup
-  Admob.init();
-  Admob.loadBannerAd();
+  // run in zone for stacktrace
+  runZoned<Future<void>>(() async {
+    WidgetsFlutterBinding.ensureInitialized();
 
-  // preload images
-  Flame.images.loadAll(<String>[
-    'bg/background.png',
-    'monster/monster.png',
-    'platform/platform.png'
-  ]);
+    // Admob setup
+    Admob.init();
+    Admob.loadBannerAd();
 
-  // Flame settings
-  Util flameUtil = Util();
-  flameUtil.fullScreen();
-  flameUtil.setOrientations(
-      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+    // preload images
+    Flame.images.loadAll(<String>[
+      'bg/background.png',
+      'monster/monster.png',
+      'platform/platform.png'
+    ]);
 
-  // run app
-  runApp(new GameContainer());
+    // Flame settings
+    Util flameUtil = Util();
+    flameUtil.fullScreen();
+    flameUtil.setOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+    // run app
+    runApp(GameContainer());
+  }, onError: Crashlytics.instance.recordError);
 }
 
 class GameContainer extends StatelessWidget {
